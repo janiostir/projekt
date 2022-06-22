fe43753a77e56284180905da1d03af336dcf464a
janiostir <jani.ostir@gmail.com>
2022-06-22 13:47:41 +0200

    CRUD - Create,Read,Update,Delete za userja

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,41 @@
-import {Controller, Get} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post, Put} from '@nestjs/common';
+import {UserService} from "./user.service";
+import {User} from "./user.entity";
 
-@Controller('users')
+@Controller('users') // \users
 export class UserController {
 
-    @Get()
+    constructor(private userService: UserService) {
+    }
+
+    @Get('users') // če dam npr 'vsi' bi rabu potem dat v urlju na koncu \vsi -> users\vsi
     all(){
-        return 'VSS je zakon!';
+        return this.userService.all();
+    }
+
+    @Post('user') // \users\user
+    create(@Body() data): Promise<User>{
+        return this.userService.create(data);
+    }
+
+    @Get(':id') // učitl je pr temu dau findOne, sam me ni delalo tako
+    getUserById(@Param('id') id: number): Promise<User>{
+        return this.userService.findOneBy({id});
+    }
+
+    @Put(':id')
+    async update(
+        @Param('id') id: number,
+        @Body() data
+    ): Promise<User>{
+        return await this.userService.update(id, data);
+    }
+
+    @Delete(':id')
+    delete(
+        @Param('id') id: number
+    ): Promise<any>{
+        return this.userService.delete(id);
     }
 
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import {User} from "./user.entity";
+import {InjectRepository} from "@nestjs/typeorm";
+import {Repository} from "typeorm";
 
 @Injectable()
 export class UserService {
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) {
+    }
 
+    async all(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
+    create(data): Promise<User> {
+        return this.userRepository.save(data);
+    }
+
+    findOneBy(condition): Promise<User> { // učitl je tu dau findOne, sam men ni delalo
+        return this.userRepository.findOneBy(condition);
+    }
+
+    async update(id, data): Promise<User> {
+        await this.userRepository.update(id, data);
+        return this.findOneBy({id});
+    }
+
+    delete(id: number): Promise<any> {
+        return this.userRepository.delete(id);
+    }
 
 }
